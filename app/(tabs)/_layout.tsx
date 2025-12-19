1ab445e4bf070229245899a51944819af0d12c25
safi islam <118913244+safiislam@users.noreply.github.com>
2025-12-20 00:04:18 +0600

    add

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,18 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Tabs } from "expo-router";
+export default function TabsLayout() {
+  return <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+    <Tabs.Screen name="index" options={{
+      title: 'Home', tabBarIcon: ({ color }) => (
+        <FontAwesome5 name="home" size={24} color={color} />
+      )
+    }} />
+    <Tabs.Screen name="login" options={{
+      title: 'Login', tabBarIcon: ({ color }) => (
+        <MaterialIcons name="login" size={24} color={color} />
+      )
+    }} />
+  </Tabs>
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
 }
---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+
+export default function LoginPage() {
+    return (
+        <>
+            <Text> login page</Text>
+        </>
+    )
+}
